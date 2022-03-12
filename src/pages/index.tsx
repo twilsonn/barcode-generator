6c77de451145319f31de11a7e1535a710bd17439
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-12 20:36:55 +0000

    update: added jotai provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { Provider } from "jotai";
 import * as React from "react";
 import Generator from "../components/Generator";
 import Sidebar from "../components/Sidebar";
 
 const IndexPage = () => {
   return (
-    <>
+    <Provider>
       <Sidebar />
       <Generator />
-    </>
+    </Provider>
   );
 };
 
