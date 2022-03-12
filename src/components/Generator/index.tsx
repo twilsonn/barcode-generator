78aed7ff11b0fec488be5fcf22a205c6f58a2f33
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-12 20:39:31 +0000

    update: added barcode generation

--- src/components/Generator/index.tsx
+++ src/components/Generator/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useAtom } from "jotai";
+import React, { createRef, useRef } from "react";
+import { barcodesAtom } from "../../store";
+import ReactBarcode from "../JsBarcode";
 
 const Generator: React.FC = () => {
-  return <main className="w-3/4 ml-[25%] min-h-screen p-4">content</main>;
+  const [barcodes] = useAtom(barcodesAtom);
+
+  return (
+    <main className="w-3/4 ml-[25%] min-h-screen p-4">
+      {barcodes.map((b) => {
+        return (
+          <ReactBarcode
+            key={b.id}
+            value={b.value}
+            options={{ format: "code128" }}
+          />
+        );
+      })}
+    </main>
+  );
 };
 
 export default Generator;
