54998cb87005191af440f57c348b0ec2350984ba
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-12 23:11:17 +0000

    added: resize hook

---
+++ src/hooks/useResize.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useResize = (myRef: React.RefObject<any>) => {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(myRef.current.offsetWidth);
+      setHeight(myRef.current.offsetHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [myRef]);
+
+  return { width, height };
+};
+
+export { useResize };
