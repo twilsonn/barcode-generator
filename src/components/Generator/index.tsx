39be014babf94967c8b4c1c2789d9bd3c8a65188
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-13 14:33:13 +0000

    update: added maxHeight value update: wrapped grid in measure update: using pintrest layout

--- src/components/Generator/index.tsx
+++ src/components/Generator/index.tsx
@@ -1,30 +1,40 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useAtom } from "jotai";
-import { barcodesAtom, maxBarcodeWidthAtom } from "../../store";
+import {
+  Barcode,
+  barcodesAtom,
+  maxBarcodeHeightAtom,
+  maxBarcodeWidthAtom,
+} from "../../store";
 
 import { useResize } from "../../hooks/useResize";
 
 import bwipJS from "bwip-js";
 
-import { SpringGrid, measureItems, CSSGrid } from "react-stonecutter";
+import { CSSGrid, layout, measureItems } from "react-stonecutter";
 
-const Barcode: React.FC<
+const Grid = measureItems(CSSGrid, { measureImages: true, background: true });
+
+const ReactBarcode: React.FC<
   React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLDivElement>,
     HTMLDivElement
-  > & { value: string; id: string }
+  > &
+    Barcode
 > = (props) => {
-  const { value, id } = props;
+  const { value, id, type, size } = props;
   const [maxWidth, setMaxWidth] = useAtom(maxBarcodeWidthAtom);
+  const [maxHeight, setMaxHeight] = useAtom(maxBarcodeHeightAtom);
+
   const [url, setUrl] = useState("");
 
   useLayoutEffect(() => {
     try {
       let canvas = document.createElement("canvas");
       bwipJS.toCanvas(canvas, {
-        bcid: "code128", // Barcode type
+        bcid: type, // Barcode type
         text: value, // Text to encode
-        scale: 2, // 3x scaling factor
+        scale: size || 2, // 3x scaling factor
         // height: 20, // Bar height, in millimeters
         includetext: true, // Show human-readable text
         textxalign: "center", // Always good to set this
@@ -32,6 +42,7 @@ const Barcode: React.FC<
 
       setUrl(canvas.toDataURL("image/png"));
       setMaxWidth(Math.max(canvas.width, maxWidth));
+      setMaxHeight(Math.max(canvas.width, maxHeight));
     } catch (e) {
       // `e` may be a string or Error object
     }
@@ -43,30 +54,37 @@ const Barcode: React.FC<
 const Generator: React.FC = () => {
   const [barcodes] = useAtom(barcodesAtom);
   const [maxWidth] = useAtom(maxBarcodeWidthAtom);
+  const [maxHeight] = useAtom(maxBarcodeHeightAtom);
 
   const ref = useRef<HTMLDivElement>(null);
   const { width, height } = useResize(ref);
 
-  const columns = Math.floor(width / (maxWidth + 32));
+  const columns = Math.floor(width / (maxWidth + 48));
 
   return (
-    <main className="generator w-3/4 ml-[25%] min-h-screen p-4">
+    <main className="generator w-3/4 ml-[25%] min-h-screen p-12">
       <div ref={ref} className="flex justify-center">
-        <CSSGrid
+        <Grid
           component="ul"
           columns={columns}
           columnWidth={maxWidth}
-          gutterWidth={32}
-          gutterHeight={32}
+          gutterWidth={48}
+          gutterHeight={48}
           duration={200}
+          layout={layout.pinterest}
           children={barcodes.map((b) => {
             return (
-              <li key={b.id}>
+              <li key={b.id} itemHeight={maxHeight}>
                 <div
                   className="flex justify-center"
                   style={{ width: maxWidth }}
                 >
-                  <Barcode id={b.id} value={b.value} />
+                  <ReactBarcode
+                    id={b.id}
+                    value={b.value}
+                    type={b.type}
+                    size={b.size}
+                  />
                 </div>
               </li>
             );
