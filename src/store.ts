11d19910c7dc123d13328456b3ab8060b08a249c
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-13 14:29:41 +0000

    update: added new barcode types

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,38 @@
 import { atom } from "jotai";
 
-const barcodeTypes = ["code128", "code39", "ean", "upc", "qr"];
-type BarcodeType = typeof barcodeTypes[number];
+const barcodeTypes = [
+  "azteccode",
+  "code128",
+  "code39",
+  "ean13",
+  "isbn",
+  "qrcode",
+  "sscc18",
+  "upca",
+];
+
+type BarcodeType =
+  | "azteccode"
+  | "code128"
+  | "code39"
+  | "ean13"
+  | "isbn"
+  | "qrcode"
+  | "sscc18"
+  | "upca";
 
 type Barcode = {
   id: string;
   type: BarcodeType;
   value: string;
+  size?: number;
+  height?: number;
 };
 
 const barcodesAtom = atom<Barcode[]>([]);
 
 const maxBarcodeWidthAtom = atom<number>(0);
+const maxBarcodeHeightAtom = atom<number>(0);
 
 if (process.env.NODE_ENV !== "production") {
   barcodesAtom.debugLabel = "barcodes";
@@ -19,4 +40,9 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 export type { Barcode, BarcodeType };
-export { barcodesAtom, barcodeTypes, maxBarcodeWidthAtom };
+export {
+  barcodesAtom,
+  barcodeTypes,
+  maxBarcodeWidthAtom,
+  maxBarcodeHeightAtom,
+};
