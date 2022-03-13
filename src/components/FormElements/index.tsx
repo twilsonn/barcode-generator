d6a3ac0b51513e395ed4e55a5d01a7e6f9410acb
Tyler Wilson <tylerwilson0729@outlook.com>
2022-03-13 14:32:03 +0000

    added: exports for all form elements

---
+++ src/components/FormElements/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Input from "./Input";
+import TextArea from "./TextArea";
+import Select from "./Select";
+import Range from "./Range";
+import Button from "./Button";
+
+export { Input, TextArea, Select, Range, Button };
